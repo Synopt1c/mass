bf9007be10d469182d5cd322f83b3c6ec8246397
Synopt1c <90967630+Synopt1c@users.noreply.github.com>
2022-03-15 10:04:20 +0300

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,49 @@
+<body>
+    <div class="tl_page_wrap">
+      <div class="tl_page">
+        <main class="tl_article">
+          <header class="tl_article_header" dir="auto">
+            <h1>Тестнет Nosana Phase 1</h1>
+            <address>
+              <a rel="author">@DremaerINC</a><!--
+           --><time datetime="2022-03-11T15:56:30+0000">March 11, 2022</time>
+            </address>
+          </header>
+          <article id="_tl_editor" class="tl_article_content"><h1>Тестнет Nosana Phase 1<br></h1><address>@DremaerINC<br></address><figure><img src="/file/15449ff9d575eb84f8107.jpg"><figcaption></figcaption></figure><h3 id="Переводим-кошелек-Phantom-в-сеть-Testnet">Переводим кошелек Phantom в сеть Testnet</h3><p>Чтобы перевести Phantom в сеть Testnet, нажмите на шестеренку:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_11-15-49.png"><figcaption></figcaption></figure><p>Выберите «Изменить сеть»:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_11-16-40.png"><figcaption></figcaption></figure><p>Выберите «Testnet»:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_11-17-10.png"><figcaption></figcaption></figure><h3 id="Получаем-тестовые-токены">Получаем тестовые токены</h3><p>Перейдите на кран <a href="https://solfaucet.com/" target="_blank">Solfaucet</a> → Укажите свой кошелек и нажмите «Testnet»:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_11-21-21.png"><figcaption></figcaption></figure><h3 id="Создаем-репозиторий-Github">Создаем репозиторий Github</h3><p>Перейдите на сайт <a href="https://github.com/" target="_blank">Github</a> и создайте аккаунт, если у Вас его нет:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_11-27-52.png"><figcaption></figcaption></figure><p>После создания аккаунта нажмите на свой аватар в правом верхнем углу и нажмите «Your profile»:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_11-29-27.png"><figcaption></figcaption></figure><p>На своей странице перейдите на вкладку «Repositories»:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_09-22-39.png"><figcaption></figcaption></figure><p>Нажмите «New», чтобы создать новый репозиторий:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_09-21-47-1.png"><figcaption></figcaption></figure><p>Дайте своему репозиторию имя, измените доступ на «Public» и нажмите «Create repository»:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_09-24-33-2.png"><figcaption></figcaption></figure><p>Далее нажмите «creating a new file», чтобы создать новый файл:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_09-26-35.png"><figcaption></figcaption></figure><p>Дайте файлу имя и напишите любой код</p><p> (можно брать любой: из других репозиториев, часть кода блокчейнов и тд)</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_09-30-38.png"><figcaption></figcaption></figure><p>В нижнем поле нажмите «Commit new file»:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_09-31-52.png"><figcaption></figcaption></figure><h3 id="Подключаем-репозиторий">Подключаем репозиторий</h3><p>Перейдите на <a href="https://testnet.nosana.io/" target="_blank">сайт Nosana</a> и нажмите «Connect Wallet»:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_09-16-13.png"><figcaption></figcaption></figure><p>Выберите кошелек «Phantom»:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_09-17-43.png"><figcaption></figcaption></figure><p>Нажмите «Login» и подпишите транзакцию:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_09-18-19-1.png"><figcaption></figcaption></figure><p>На главной странице нажмите «Add Repository»:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_09-34-45-1.png"><figcaption></figcaption></figure><p>Войдите с помощью Github, нажав «Authorize nosana-ci»:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_09-35-28-1.png"><figcaption></figcaption></figure><p>Выберите репозиторий из списка и нажмите «Add...»:</p><blockquote>&quot;Если у вас есть личные проекты, то рекомендуем добавить их, тем самым шанс получения аирдропа будет выше&quot;</blockquote><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_09-37-17.png"><figcaption></figcaption></figure><p>На главной странице нажимаем «Request Funds»:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_09-38-00-1.png"><figcaption></figcaption></figure><p>В форме пишем свое имя и почту, также можете добавить описание проекта и свой Discord никнейм:</p><figure><img src="https://blog.crypton.academy/content/images/2022/03/2022-03-07_09-39-23-1.png"><figcaption></figcaption></figure><p>Подписываемся на <a href="https://discord.gg/gkRRqAGta4" target="_blank">Discord проекта</a> и <a href="https://docs.google.com/forms/d/e/1FAIpQLSdxLuJjSmo9kvgps8QUN5hLgmoWaPnIpxL4C0OTPknEstBBbg/viewform" target="_blank">заполняем форму</a> и даем заявку на участие во второй фазе тестнета</p><p>Даем своей фидбэк в <a href="https://discord.gg/gkRRqAGta4" target="_blank">дискорд</a> в разделе &quot;testnet-launch&quot;</p><p>На данный момент это все, что можно сделать. Инструкция будет пополняться по мере обновления тестнета.</p><p>После Фазы 1 будет Фаза 2, рекомендуем заполнить эту <a href="https://docs.google.com/forms/d/e/1FAIpQLSdxLuJjSmo9kvgps8QUN5hLgmoWaPnIpxL4C0OTPknEstBBbg/viewform" target="_blank">форму</a> для участия во Второй Фазе тестнета.</p><h3 id="Что-ожидать-от-тестнета?">Что ожидать от тестнета?</h3><p>По словам разработчиков, выбранные проекты получат по 500 &#036;NOS за присоединение к тестовой сети, также можно заработать до 10000 &#036;NOS за проявление активности в тестнете. Эти токены будут в тестовой сети, но они перейдут в основную сеть после запуска Nosana в мейннете.</p><h3 id="Спасибо-за-внимание&amp;#33;">Спасибо за внимание&#33;</h3><p><br></p><p><br></p><p><br></p></article>
+          <div id="_tl_link_tooltip" class="tl_link_tooltip"></div>
+          <div id="_tl_tooltip" class="tl_tooltip">
+            <div class="buttons">
+              <span class="button_hover"></span>
+              <span class="button_group"><!--
+             --><button id="_bold_button"></button><!--
+             --><button id="_italic_button"></button><!--
+             --><button id="_link_button"></button><!--
+           --></span><!--
+           --><span class="button_group"><!--
+             --><button id="_header_button"></button><!--
+             --><button id="_subheader_button"></button><!--
+             --><button id="_quote_button"></button><!--
+           --></span>
+            </div>
+            <div class="prompt">
+              <span class="close"></span>
+              <div class="prompt_input_wrap"><input type="url" class="prompt_input" /></div>
+            </div>
+          </div>
+          <div id="_tl_blocks" class="tl_blocks">
+            <div class="buttons">
+              <button id="_image_button"></button><!--
+           --><button id="_embed_button"></button>
+            </div>
+          </div>
+          <aside class="tl_article_buttons">
+            <div class="account account_top"></div>
+            <button id="_edit_button" class="button edit_button">Edit</button><!--
+         --><button id="_publish_button" class="button publish_button">Publish</button>
+            <div class="account account_bottom"></div>
+            <div id="_error_msg" class="error_msg"></div>
+          </aside>
+        </main>
+      </div>
+    </div>
+    <div id="_tl_alerts" class="tl_alerts"></div>
